9ab651fb4c730b2fb067f4efe9d42e06dd65d358
Gabriel Nunes <gabriellopes.nunes@hotmail.com>
2019-05-20 01:09:49 -0300

    Fixes KBProcessor

---
+++ src/main/java/br/unisinos/parthenos/generator/processor/pojoui/PojoUIKnowledgeBaseProcessor.java
@@ -0,0 +1,8 @@
+package br.unisinos.parthenos.generator.processor.pojoui;
+
+import br.unisinos.parthenos.generator.annotation.Language;
+import br.unisinos.parthenos.generator.processor.java.JavaKnowledgeBaseProcessor;
+
+@Language("pojo-ui")
+public class PojoUIKnowledgeBaseProcessor extends JavaKnowledgeBaseProcessor {
+}
